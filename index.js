f00254483b1cc02ef60edfb971cad59cfaa0e462
AlexC2103 <80043678+AlexC2103@users.noreply.github.com>
2021-04-18 12:31:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var randomID = Math.floor(Math.random() * 3) + 1;
-var CorrectButtons = 0;
-var WrongButtons = 0;
+var correctButtons = 0;
+var wrongButtons = 0;
 function replyClick(clickedID) {
   if (clickedID == randomID) {
-    document.getElementById(clickedID).style.background = 'green';
-    CorrectButtons++;
+    document.getElementById(clickedID).style.background = '#58c75b';
+    correctButtons++;
   } else {
-    document.getElementById(clickedID).style.background = 'red';
-    WrongButtons++;
+    document.getElementById(clickedID).style.background = '#e04f4f';
+    wrongButtons++;
   }
 
   for (var i = 1; i <= 3; ++i) {
     if (i != clickedID) {
       document.getElementById(i).disabled = true;
       if (i == randomID) {
-        document.getElementById(i).style.background = 'green';
+        document.getElementById(i).style.background = '#58c75b';
       } else {
-        document.getElementById(i).style.background = 'red';
+        document.getElementById(i).style.background = '#e04f4f';
       }
     }
   }
 }
 
-function Restart() {
+function restart() {
   for (var i = 1; i <= 3; ++i) {
     document.getElementById(i).style.background = '';
     document.getElementById(i).disabled = false;
@@ -31,8 +31,7 @@ function Restart() {
   randomID = Math.floor(Math.random() * 3) + 1;
 }
 
-function EndGame() {
-  alert('Correct Buttons: ' + CorrectButtons + '\nWrong Buttons: ' + WrongButtons);
-  Restart();
-  WrongButtons = CorrectButtons = 0;
+function endGame() {
+  restart();
+  wrongButtons = correctButtons = 0;
 }
